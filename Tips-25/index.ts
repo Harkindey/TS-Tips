67d29766daa22b2956e86f4337e5ccb58b27d39c
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-11 00:08:01 +0100

    Compare function overloads and generics

---
+++ Tips-25/index.ts
@@ -0,0 +1,12 @@
+// export function returnWhatIPassIn(input: string): string
+// export function returnWhatIPassIn(input: number): number
+
+export function returnWhatIPassIn<TInput>(input: TInput): TInput {
+    return input;
+}
+
+const result = returnWhatIPassIn(12)
+const result2 = returnWhatIPassIn({
+    name: 'bean',
+    age: 12,
+})
