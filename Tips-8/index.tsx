0638134ad397f11d0133abbee6c1809634eccf61
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-10 02:04:38 +0100

    Use generics in React to make dynamic and flexible components

--- Tips-8/index.tsx
+++ Tips-8/index.tsx
@@ -9,12 +9,21 @@ export function Table<TItem>(props: TableProps<TItem>)  {
     return null
 }
 
+type Contacts = {
+    id: number;
+    name: string;
+    phone: string;
+    address: string;
+}
+
 const Component = () => {
     return (
-        <Table
+        <Table<Contacts>
             items={[{
-                id: "1",
-                name: 'bean'
+                id: 1,
+                name: 'bean',
+                phone: "",
+                address: ""
             }]}
             renderItem={(item)=> <div>{item.id}</div>}
         ></Table>
